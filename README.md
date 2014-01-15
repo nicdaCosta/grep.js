858987ec84ee6a8e0917e059e64ee0ce41eb0709
Nic da Costa <nic@dsgnrhead.com>
2014-01-15 19:36:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+##Grep.js
+
+_Best used when saved as a snippet. To do so in Chrome Dev Tools, [read here](https://developers.google.com/chrome-developer-tools/docs/authoring-development-workflow#snippets)_
+
+This is a basic function which allows you to search through an object's properties and find a property based on a match. This is particularly useful when searching for a given DOM API and if you are unsure if it is vendor prefixed.
+
+###Overview
+Basic function that searches / filters any object or function and returns matched properties.
+
+This receives either a string or regex as the initial parameter( strSearch ), the sencond parameter ( _isRecursive == Bool_ ) is used to determine whether to search the property's properties should the current property be of type object / function. 
+
+Main area of use would be for DevTools purposes, when needing to find a specific property but only part of the property name is known. 
+Is now an inherited method on any Object / Function due to Object.prototype.
+
+This method returns an object of all matches and their relative properties. Should not match be found, each property of the given object that is an object itself will be checked for any matches.
+
+###Usage 
+
+```Object.Grep( 'SearchTerm' ).``` _* in browser console_
+
+eg:
+
+```js
+ navigator.Grep( 'geo' ); 
+ // returns an object of matched results and their relative properties
+```
+
+Should you wish to use this with an interface as opposed to having to use the console, [http://object.hasownproperty.com](http://object.hasownproperty.com) can be used.
+
+### License
+Copyright (c) 2014 Nic da Costa  
+Licensed under the [MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ grep.js
@@ -0,0 +1,83 @@
+/*
+	Grep.js
+	Author	: Nic da Costa ( @nic_daCosta )
+        Created : 2012/11/14
+	Version	: 0.2
+	(c) Nic da Costa
+	License : MIT, GPL licenses
+
+	Overview:
+	Basic function that searches / filters any object or function and returns matched properties.
+	This receives either a string or regex as the initial parameter( strSearch ), the sencond parameter ( isRecursive == Bool) is
+	used to determine whether to search the property's properties should the current property be of type object / function. 
+	Main area of use would be for DevTools purposes, when needing to find a specific property but only part of 
+		the property name is known. 
+		Is now an inherited method on any Object / Function due to Object.prototype.
+		Thus can go ObjectName.Grep( 'SearchTerm' ).
+		eg: navigator.Grep( 'geo' );
+			var foo = function(){}; foo.Grep( 'proto' );
+
+       *Tested in Chrome Dev Tools*
+
+*/
+
+
+Object.prototype.Grep = function( strSearch , isRecursive ) {
+
+	// Checks if seach string is not empty/undefined
+	if ( !strSearch ) {
+
+		return this;
+
+	}
+
+	// Used to prevent maxing out callstack for sub-lookups due to __proto__ == Object
+	isRecursive = isRecursive || false;
+
+	// Declare necessary local variables to hold necessary values
+	var objToIterate = this,
+		typeOfObject = typeof objToIterate,
+		objKeys = [],
+		objResult = {};
+
+	
+	// if item that needs to be iterated over is an object or function, get all properties ( including non enumerable properties )
+	if ( typeOfObject === 'object' || typeOfObject === 'function' ) {
+	
+		objKeys = Object.getOwnPropertyNames( objToIterate );
+
+	}
+
+	// Loop through all the properties
+	objKeys.forEach( function( item ) {
+
+		var itemValue;
+	
+		/* 
+			Initially check if search phrase is a regular expression, if so check if there is a match, else
+			check if key matches search string, if so add, if not, check if object and iterate through object's keys
+		*/
+		if ( ( strSearch instanceof RegExp ) ? item.match( strSearch ) : item.toLowerCase().indexOf( strSearch.toLowerCase() ) >= 0 ) {
+
+			itemValue = objToIterate[ item ];
+
+		}
+		else if ( typeof objToIterate[ item ] === 'object' && !isRecursive ){
+
+			itemValue = Grep.call( objToIterate[ item ] , strSearch , true );
+
+		}
+
+		// Check if Item Value has a value, if so, add to results
+		if ( itemValue ) {
+
+			objResult[ item ] = itemValue;
+
+		}
+
+	} );
+
+	// checks if objResult is empty, if so, return empty string.
+	return ( Object.getOwnPropertyNames( objResult ).length ) ? objResult : '';
+
+};
